4329aaf0566d1f12ff399efed62e0bc0f94652f7
Huy Dang <ghuydang@gmail.com>
2025-04-18 00:27:35 -0700

    chore(nvim): remove custom move line keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,9 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-
--- These keymaps are made for Colemak
+--[[
+Keymaps are automatically loaded on the VeryLazy event
+Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+The following custom keymaps are made for Colemak, but you should try to maintain the default nvim keymaps
+even if it's slighty strange for Colemak.
+--]]
 
 -- Focus navigation (n,e,i,o instead of h,j,k,l)
 vim.keymap.set("n", "<C-w>n", "<C-w>h", { desc = "Move to the left window" })
@@ -9,22 +11,16 @@ vim.keymap.set("n", "<C-w>e", "<C-w>j", { desc = "Move to the window below" })
 vim.keymap.set("n", "<C-w>i", "<C-w>k", { desc = "Move to the window above" })
 vim.keymap.set("n", "<C-w>o", "<C-w>l", { desc = "Move to the right window" })
 
--- Move selected lines up and and down
--- macOS: Check that C-Up and C-Down aren't current bound to system keyboard shortcut.
-vim.keymap.set("n", "<C-Up>", ":m .-2<CR>==", { desc = "Move line up" })
-vim.keymap.set("n", "<C-Down>", ":m .+1<CR>==", { desc = "Move line down" })
-vim.keymap.set("i", "<C-Up>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
-vim.keymap.set("i", "<C-Down>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
-vim.keymap.set("v", "<C-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-vim.keymap.set("v", "<C-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+-- Swap Alt-j and Alt-k for moving lines up and down.
+-- Alt-j to move up (default down)
+vim.keymap.set("n", "<A-j>", "<cmd>m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("v", "<A-j>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("i", "<A-j>", "<Esc><cmd>m .-2<CR>==gi", { desc = "Move line up" })
+-- Alt-k to move down (default up)
+vim.keymap.set("n", "<A-k>", "<cmd>m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("v", "<A-k>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("i", "<A-k>", "<Esc><cmd>m .+1<CR>==gi", { desc = "Move line down" })
 
 -- Bufferline Pick
 vim.keymap.set("n", "<leader>bg", ":BufferLinePick<CR>", { desc = "Switch to a tab" })
 vim.keymap.set("v", "<leader>bg", "<ESC>:BufferLinePick<CR>", { desc = "Switch to a tab" })
-
--- Reload keymaps
-vim.keymap.set("n", "<leader>rr", function()
-	package.loaded["config.keymaps"] = nil
-	require("config.keymaps")
-	vim.notify("Keymaps reloaded!", vim.log.levels.INFO)
-end, { desc = "Reload keymaps" })
