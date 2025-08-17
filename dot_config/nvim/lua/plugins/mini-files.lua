141756afb7c17e6dfef85861e91f37e6fe53c434
Huy Dang <ghuydang@gmail.com>
2025-08-17 16:24:29 -0700

    New nvim configuration from scratch

---
+++ dot_config/nvim/lua/plugins/mini-files.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-mini/mini.files",
+  version = "*",
+  opts = {
+    mappings = {
+      close = "q",
+      go_in = "<Right>", -- go into directory or open file
+      go_in_plus = "<S-Right>",
+      go_out = "<Left>",
+      go_out_plus = "<S-Left>",
+      mark_goto = "'",
+      mark_set = "m",
+      reset = "<BS>",
+      reveal_cwd = "@",
+      show_help = "g?",
+      synchronize = "=",
+      trim_left = "<",
+      trim_right = ">",
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "snacks.nvim",
+  opts = {
+    explorer = { enabled = false },
+  },
+  keys = {
+    -- Disable explorer keymaps
+    { "<leader>e", false },
+    { "<leader>E", false },
+    { "<leader>fe", false },
+    { "<leader>fE", false },
+  },
+}
